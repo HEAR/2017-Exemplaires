89ce90c01f0fe5d141a43f4f691a3a25e2b22229
Loïc Horellou <hello@loichorellou.net>
2016-12-25 17:46:31 +0100

    update css + images

--- 503.php
+++ 503.php
@@ -3,26 +3,33 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Exemplaires 2017</title>
-	<link rel="stylesheet" href="<?php bloginfo( 'template_url' ); ?>/fonts/Academy/stylesheet.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Exemplaires 2017, formes et pratiques de l’édition</title>
 	<link rel="stylesheet" href="<?php bloginfo( 'template_url' ); ?>/503.css">
 </head>
 <body>
 
-	<h1>Exemplaires</h1>
-	<p>30.03 – 01.04.2017<br>
-	Colloque</p>
-	<p>Exposition<br>
-	30.03 – 30.04.2017</p>	
-	<h2>Formes<br>
-	et pratiques<br>
-	de l’édition</h2>
-	<p>2e Biennale</p>
-	<p>Strasbourg</p>
-	<p class="small">Site en construction</p>
-	<p>Haute<br>école<br>des arts<br>du Rhin</p>
-	<p class="small">écoles participantes : École européenne supérieure d’art de Bretagne | École nationale supérieure des beaux-arts de Lyon | École de recherche graphique de Bruxelles | École supérieure d’art et design – Grenoble, Valence | Haute école d’art et de design – Genève | Haute école des arts du Rhin | Institut supérieur des arts de Toulouse | Institut supérieur des Beaux-Arts de Besançon | Université du Québec à Montréal<br>
-	En association avec : Université Paris 8 – département Arts plastiques | Université de Strasbourg – Master design</p>
+	<div id="page">
+
+		<h1>Exemplaires,</h1>
+
+		<div class="inner">
+
+			<h2>formes et pratiques<br>de l’édition</h2>
+
+			<div class="row">
+				<h4><span>2<sup>e</sup> biennale<br>Strasbourg</span></h4>
+				<h3 id="colloque"><span>30.03 –<br>01.04.2017<br>colloque</span></h3>
+
+			</div>
+
+			<div class="row">
+				<h3 id="exposition"><span>30.03 –<br>30.04.2017<br>exposition</span></h3>
+				<h5><a href="http://www.hear.fr/" target="_blank"><span>Haute école<br>des arts<br>du Rhin</a></span></h5>
+			</div>
+
+		</div>
+	</div>
 	
 </body>
 </html>
