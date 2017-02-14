e0e806bc71789a2a673d954ee66f66f707202658
marianneplano <marianne.plano@yahoo.fr>
2017-02-14 17:54:22 +0100

    Add files via upload

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -30,6 +30,11 @@
 <?php endforeach ?>
 
 </ul>  -->
+      <?php foreach($pages->visible()->filterBy('url','*=','presentation') as $item):?>
+      <li class="menu-item<?= r($item->isOpen(), ' is-active') ?>" id="<?php echo  $item->id() ?>">
+         <a href="<?= $item->url() ?>"><?= $item->title()->html() ?></a>
+      </li>
+      <?php endforeach ?>
 
 
       <li class="menu-item" id="liste-programme">	
@@ -40,33 +45,34 @@
       <ul class="sous-menu invisible" id="programme">
          <?php foreach($pages->visible()->filterBy('url', '*=', 'programme-') as $item):?>
          <li class="menu-item<?= r($item->isOpen(), ' is-active') ?>">
-            <a href="<?= $item->url() ?>">
-               <table><tr>
-                  <td><span class='puce'>&#149;&thinsp;</span></td>
-                  <td><?= $item->title()->html() ?></td>
-                  </tr></table></a>
+            <a href="<?= $item->url() ?>"><?= $item->title()->html() ?></a>
          </li>
          <?php endforeach ?>
 
       </ul>
       <?php 
-   $excluded=$pages->visible()->filterBy('url', '*=', 'programme');
+   $ot=array($pages->visible()->filterBy('url', '*=', 'programme'), $pages->visible()->filterBy('url', '*=', 'presentation'));
 
-             foreach($pages->visible()->filterBy('template','default')->not($excluded) as $item):?>
+       foreach( $pages->visible()->filterBy('template','default')->not($ot) as $item):?>
+      <li class="menu-item<?= r($item->isOpen(), ' is-active') ?>" id="<?php echo  $item->id() ?>">
+         <a href="<?= $item->url() ?>"><?= $item->title()->html() ?></a>
+      </li>
+      <?php endforeach; 
+
+             foreach( $pages->visible()->filterBy('template','contact') as $item):?>
       <li class="menu-item<?= r($item->isOpen(), ' is-active') ?>" id="<?php echo  $item->id() ?>">
          <a href="<?= $item->url() ?>"><?= $item->title()->html() ?></a>
       </li>
       <?php endforeach ?>
 
-      <?php 
-   $excluded=$pages->visible()->filterBy('url', '*=', 'programme');
-
-          foreach($pages->visible()->filterBy('template','contact')->not($excluded) as $item):?>
-      <li class="menu-item<?= r($item->isOpen(), ' is-active') ?>" id="<?php echo  $item->id() ?>">
-         <a href="<?= $item->url() ?>"><?= $item->title()->html() ?></a>
-      </li>
-      <?php endforeach ?>
    </ul>
+      <ul class='liens_ext'>
+        <li>
+      <a href="https://www.facebook.com/Exemplaires2017/" target="_blank"><i class="fa fa-facebook fa-lg"></i></a>
+      <a href="https://www.instagram.com/Exemplaires2017/" target="_blank"><i class="fa fa-instagram fa-lg"></i></a>
+      </li>
+      <li><a href="https://exemplaires2015.fr/" target="_blank">Exemplaires</br>2015</a></li>
+      </ul>
 </nav>
 
 
