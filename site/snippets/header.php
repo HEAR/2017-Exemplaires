42117030c01394b4ae419e987fa7972124fe101b
Loïc Horellou <hello@loichorellou.net>
2017-03-28 18:57:32 +0200

    update meta

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -4,9 +4,30 @@
 
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<meta name="description" content="<?php echo $site->description()->html() ?>">
+ 	<meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
+ 	<meta name="author" content="<?php echo $site->author()->html() ?>">
 
 	<title><?= $site->title()->html() ?>&nbsp;2017 | <?= $page->title()->html() ?></title>
 	<meta name="description" content="<?= $site->description()->html() ?>">
+
+	<meta property="og:site_name" content="<?php echo $site->title()->html() ?>">
+	<meta property="og:title" content="<?php echo $page->title()->html() ?>">
+	<meta property="og:description" content="<?php echo $site->description()->html() ?>">
+	<meta property="og:url" content="<?php echo $site->url() ?>">
+	<meta property="og:image" content="<?php echo $site->ogimage()->html() ?>">
+	<meta property="og:type" content="website">
+
+	<meta name="twitter:title" content="<?php echo $page->title()->html() ?>">
+	<meta name="twitter:description" content="<?php echo $site->description()->html() ?>">
+	<meta name="twitter:url" content="<?php echo $site->url() ?>">
+	<meta name="twitter:image" content="<?php echo $site->ogimage()->html() ?>">
+	<meta name="twitter:card" content="summary">
+	<meta name="twitter:site" content="<?php echo $site->title()->html() ?>">
+	<meta name="twitter:creator" content="<?php echo $site->author()->html() ?>">
+
+	<meta name="apple-mobile-web-app-capable" content="yes">
+
 	<?= css('assets/css/index.css') ?>
 	<?= css('assets/css/font-awesome.css') ?>
 
