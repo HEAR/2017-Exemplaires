a26e9d9e11b73c560c1de206359d0141fa09088d
Loïc Horellou <hello@loichorellou.net>
2017-04-28 15:51:11 +0200

    update booklist

--- site/snippets/booklist.php
+++ site/snippets/booklist.php
@@ -2,57 +2,59 @@
 
 <?php $pxtorem=16; ?>
 
-  <div class="side">
- 
-
-   
-
-            <?php foreach(page()->children()->visible()->limit(1)  as $livre){ ?>
-
-      
-          <h2 id="i<?= $livre->images()->first()->name() ?>"><a href="<?= $livre->url() ?>"><?php echo $livre->title()->html()  ?></a></h2>
-  
-    <?php }  ?>
-
-   <h2>Selection</h2>
-      <ul>
+<div class="side">
 
 
-        <?php foreach(page()->children()->visible()->not('catalogue')->sortBy('title', 'asc')  as $livre){ ?>
-        <li id="i<?= $livre->images()->first()->name() ?>">
-          <h3><a href="<?= $livre->url() ?>"><?php echo $livre->title()->kirbytext()  ?></a></h3>
-        </li>
-        <?php }  ?>
+	<!--<h2>Publication d'accompagnement</h2>-->
 
-      </ul>
-   </div>
+	<?php foreach(page()->children()->visible()->limit(1)  as $livre){ ?>
 
-  <div class="main">
-    <h2><?= $page->title()->html() ?></h1>
-    <h3><?= $page->theme()->html() ?></h2>
-    <p><?= $page->about()->html() ?></p>
 
-    <ul>
+	<h2 id="i<?= $livre->images()->first()->name() ?>"><a href="<?= $livre->url() ?>"><?php echo $livre->title()->html()  ?></a></h2>
 
-              <?php foreach(page()->children()->visible()->limit(1)  as $livre){ ?>
-       <li id="i<?= $livre->images()->first()->name() ?>">
-          <a href="<?= $livre->url() ?>">
-            <img class="gray" src="<?= $livre->images()->first()->resize(800, 800)->url() ?>" height="<?= (($livre->height()->value()/10)*$pxtorem)?>">
-         </a>
-       </li>
-  <?php }  ?>
+	<?php }  ?>
 
-<li class="filet"></li>
+	<h2>Ouvrages sélectionnés</h2>
+	<ul>
+		<?php foreach(page()->children()->visible()->not('catalogue')->sortBy('title', 'asc')  as $livre){ ?>
+		<li id="i<?= $livre->images()->first()->name() ?>">
+			<h3><a href="<?= $livre->url() ?>"><?php echo $livre->title()->kirbytext()  ?></a></h3>
+		</li>
+		<?php }  ?>
 
-      <?php foreach($page->children()->visible()->not('catalogue')->sortBy('title', 'asc') as $livre){ ?>
-        <li id="i<?= $livre->images()->first()->name() ?>">
-          <a href="<?= $livre->url() ?>">
-            <img class="gray" src="<?= $livre->images()->first()->resize(800, 800)->url() ?>" height="<?= (($livre->height()->value()/10)*$pxtorem)?>">
-         </a>
-       </li>
- 
-   <?php } ?>
-       </ul>
-  </div>
+	</ul>
+
+	<!--<h2>Ouvrages de comparaisons</h2>-->
+</div>
+
+<div class="main">
+	<h2><?= $page->title()->html() ?></h2>
+	<h3><?= $page->theme()->html() ?></h3>
+	<p><?= $page->about()->html() ?></p>
+
+	<ul>
+
+		<?php foreach(page()->children()->visible()->limit(1)  as $livre){ ?>
+		<li id="i<?= $livre->images()->first()->name() ?>">
+			<a href="<?= $livre->url() ?>">
+				<img class="gray" src="<?= $livre->images()->first()->resize(800, 800)->url() ?>" height="<?= (($livre->height()->value()/10)*$pxtorem)?>">
+			</a>
+		</li>
+		<?php }  ?>
+
+		<li class="filet"></li>
+
+		<?php foreach($page->children()->visible()->not('catalogue')->sortBy('title', 'asc') as $livre){ ?>
+		<li id="i<?= $livre->images()->first()->name() ?>">
+			<a href="<?= $livre->url() ?>">
+				<img class="gray" src="<?= $livre->images()->first()->resize(800, 800)->url() ?>" height="<?= (($livre->height()->value()/10)*$pxtorem)?>">
+			</a>
+		</li>
+
+		<?php } ?>
+	</ul>
+
+	<!--<h2>Ouvrages de comparaisons</h2>-->
+</div>
 
 <!-- fin snippets/booklist.php -->
